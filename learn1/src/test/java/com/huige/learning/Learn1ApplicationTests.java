53cbf11ba3cc79dff953d6e2a09dc073c1639be3
hellheart <809547591@qq.com>
2022-06-01 12:02:20 +0800

    提交代码了

---
+++ learn1/src/test/java/com/huige/learning/Learn1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huige.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Learn1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
